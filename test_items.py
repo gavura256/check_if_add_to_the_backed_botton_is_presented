b56755f54638bab184ea8bd9e46bdd01993751b4
Kostiantyn Gavura <kostya.gavura@gmail.com>
2021-09-22 19:39:28 +0200

    name corrections

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-def test_is_the_product_present_in_the_shopping_cart(browser, language):
+def test_is_add_to_backed_button_present_on_the_page(browser, language):
     link = 'http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/'.format(language)
     browser.get(link)
     add_to_backed_button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
